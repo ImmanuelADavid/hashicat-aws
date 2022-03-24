08f38e1bac1074491552f841a1b4db2781abab89
Immanuel David <immanuel.david@tcs.com>
2022-03-24 20:58:53 +0000

    Added s3 bucket module with 2.8.0

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-    source = "terraform-aws-modules/s3-bucket/aws"
+    source = "terraform-aws-modules/s3-bucket/aws/2.8.0"
 
     bucket = "my-s3-bucket"
     acl = "private"
